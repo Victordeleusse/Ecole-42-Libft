2728246e1890741c2f8a6f08f9b3eceb5b777b74
Your Name <you@example.com>
2022-11-14 10:05:22 +0100

    Calloc a jour

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vde-leus <vde-leus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 15:27:17 by vde-leus          #+#    #+#             */
-/*   Updated: 2022/11/10 18:56:49 by vde-leus         ###   ########.fr       */
+/*   Updated: 2022/11/14 09:48:04 by vde-leus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*str;
 
-	if ((nmemb != 0 && size > (SIZE_MAX / nmemb)))
+	if ((nmemb != 0 && size > ((size_t) -1 / nmemb)))
 		return (NULL);
 	str = (void *)malloc(nmemb * size);
 	if (!str)
